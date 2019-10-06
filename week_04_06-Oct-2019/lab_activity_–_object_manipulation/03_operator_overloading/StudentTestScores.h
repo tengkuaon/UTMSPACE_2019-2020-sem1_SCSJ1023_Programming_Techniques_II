5ee05be9421fe112b376be09b502d46c18bff71a
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2019-10-06 16:54:36 +0800

    Updated header file.

--- week_04_06-Oct-2019/lab_activity_–_object_manipulation/03_operator_overloading/StudentTestScores.h
+++ week_04_06-Oct-2019/lab_activity_–_object_manipulation/03_operator_overloading/StudentTestScores.h
@@ -51,25 +51,11 @@ class StudentTestScores
         //     }
         // }
 
-        // const StudentTestScores operator= (const StudentTestScores &right)
-        // {
-        //     delete [] testScores;
-        //     studentName = right.studentName;
-        //     numTestScores = right.numTestScores;
-        //     testScores = new double [numTestScores];
-        //     for (int i = 0; i < numTestScores; ++i)
-        //     {
-        //         testScores[i] = right.testScores[i];
-        //     }
-        //     return *this;
-        // }
-
         const StudentTestScores operator= (const StudentTestScores &right)
         {
             delete [] testScores;
             studentName = right.studentName;
-            numTestScores = right.numTestScores;
-            
+            numTestScores = right.numTestScores;            
             testScores = new double [numTestScores];
             for (int i = 0; i < numTestScores; ++i)
             {
